9d15a292371c9e0a34b408fd8899d12eaf0ef6c1
Frederico Ferreira <fredericodavin@gmail.com>
2023-09-20 01:19:47 -0300

    feat: add colored comparison items

--- Contents/mods/BetterClothingInfoComparison/media/lua/client/bcic_tooltip.lua
+++ Contents/mods/BetterClothingInfoComparison/media/lua/client/bcic_tooltip.lua
@@ -1,7 +1,6 @@
 -- author: Bruno Menezes
 -- version: 0.1a (2023-09-19)
 -- based on: 41+
-
 require "ISUI/ISToolTipInv"
 
 -- set width for pixel for fonts
@@ -70,7 +69,12 @@ function ISToolTipInv:bcic_render()
         local th = self.tooltip:getHeight();
 
         if self.followMouse then
-            self:adjustPositionToAvoidOverlap({ x = mx - 24 * 2, y = my - 24 * 2, width = 24 * 2, height = 24 * 2 });
+            self:adjustPositionToAvoidOverlap({
+                x = mx - 24 * 2,
+                y = my - 24 * 2,
+                width = 24 * 2,
+                height = 24 * 2
+            });
         end
 
         self:drawRect(0, 0, self.width, self.height, self.backgroundColor.a, self.backgroundColor.r,
@@ -78,8 +82,9 @@ function ISToolTipInv:bcic_render()
         self:drawRectBorder(0, 0, self.width, self.height, self.borderColor.a, self.borderColor.r, self.borderColor.g,
             self.borderColor.b);
 
-        if self.item:IsClothing() then
-            tw, th = RenderTooltipClothing(self.tooltip, self.item, tw, th);
+        local playerObj = self.tooltip:getCharacter();
+        if self.item:IsClothing() and not self.item:isEquipped() then
+            tw, th = RenderTooltipClothing(self.tooltip, self.item, tw, th, playerObj);
         end
 
         self.tooltip:setX(math.max(0, math.min(mx + 11, maxX - tw - 1)));
@@ -100,20 +105,50 @@ end
 
 ISToolTipInv.render = ISToolTipInv.bcic_render;
 
-function RenderTooltipClothing(tooltipContext, itemContext, tw, th)
+function RenderTooltipClothing(tooltipContext, itemContext, tw, th, playerObj)
     y_position = tooltipContext:getHeight() + lineHeight * 0.2;
 
-    -- item statistcs
-    RenderInfo("Item Statistcs", nil, nil, tooltipContext);
+    -- AVALIAR NECESSIDADE DE INCLUIR REPLACE_ITEMS
+    local previousCondition = 0;
+    local previousInsulation = 0;
+    local previousWindResistance = 0;
+    local previousBiteDefense = 0;
+    local previousScratchDefense = 0;
+    local previousHolesNumber = 0;
+    local wornItems = playerObj:getWornItems();
+    local bodyLocationGroup = wornItems:getBodyLocationGroup()
+    local location = itemContext:IsClothing() and itemContext:getBodyLocation() or itemContext:canBeEquipped()
 
-    -- condition
-    RenderInfo("Tooltip_weapon_Condition", itemContext:getCondition(), itemContext:getConditionMax(), tooltipContext);
+    for i = 1, wornItems:size() do
+        local wornItem = wornItems:get(i - 1)
+        local item = wornItem:getItem()
+        if (itemContext:getBodyLocation() == wornItem:getLocation()) or
+            bodyLocationGroup:isExclusive(location, wornItem:getLocation()) then
+            if item:IsClothing() then
+                previousCondition = previousCondition + item:getCondition();
+                previousInsulation = previousInsulation + item:getInsulation();
+                previousWindResistance = previousWindResistance + item:getWindresistance();
+                previousBiteDefense = previousBiteDefense + item:getBiteDefense();
+                previousScratchDefense = previousScratchDefense + item:getScratchDefense();
+                previousHolesNumber = previousHolesNumber + item:getHolesNumber();
+            end
+        end
+    end
 
-    -- insulation
-    RenderInfo("Tooltip_item_Insulation", itemContext:getInsulation(), nil, tooltipContext);
+    local newCondition = itemContext:getCondition();
+    local newInsulation = itemContext:getInsulation();
+    local newWindResistance = itemContext:getWindresistance();
+    local newBiteDefense = itemContext:getBiteDefense();
+    local newScratchDefense = itemContext:getScratchDefense();
+    local newHolesNumber = itemContext:getHolesNumber();
 
-    --wind resistance
-    RenderInfo("Tooltip_item_Windresist", itemContext:getWindresistance(), nil, tooltipContext);
+    formatTooltipInv(tooltipContext, nil, nil, nil, "Item Comparison", false);
+    formatTooltipInv(tooltipContext, previousCondition, newCondition, 10, "Tooltip_weapon_Condition", false);
+    formatTooltipInv(tooltipContext, previousInsulation, newInsulation, 100, "Tooltip_item_Insulation", false);
+    formatTooltipInv(tooltipContext, previousWindResistance, newWindResistance, 100, "Tooltip_item_Windresist", false);
+    formatTooltipInv(tooltipContext, previousBiteDefense, newBiteDefense, 1, "Tooltip_BiteDefense", false);
+    formatTooltipInv(tooltipContext, previousScratchDefense, newScratchDefense, 1, "Tooltip_ScratchDefense", false);
+    formatTooltipInv(tooltipContext, previousHolesNumber, newHolesNumber, 1, "Tooltip_clothing_holes", true);
 
     -- adjust window
     th = y_position;
@@ -123,35 +158,37 @@ function RenderTooltipClothing(tooltipContext, itemContext, tw, th)
     return tw, th;
 end
 
-function RenderInfo(label, value1, value2, tooltipContext)
+function formatTooltipInv(tooltipContext, previous, new, multiplicationFactor, labelTooltip, invertHC)
     y_position = y_position + 15;
-    label = getText(label);
-    value1 = GetValue(value1);
-    value2 = GetValue(value2);
+    local text = "";
+    local hc = {
+        getR = function()
+            return 224;
+        end,
+        getG = function()
+            return 224;
+        end,
+        getB = function()
+            return 224;
+        end
+    }
 
-    local value = "";
-    
-    if value1 ~= nil and value2 ~= nil then
-        label = label .. ":";
-        value = tostring(value1) .. " / " .. tostring(value2);
-    elseif value1 ~= nil and value2 == nil then
-        label = label .. ":";
-        value = value1;
-    elseif value1 == nil and value2 ~= nil then
-        label = label .. ":";
-        value = value2;
+    if previous ~= nil or new ~= nil then
+        local plus = "";
+        if previous > 0 and previous > new then
+            hc = invertHC and getCore():getGoodHighlitedColor() or getCore():getBadHighlitedColor();
+            plus = "";
+        end
+        if new > 0 and new > previous then
+            hc = invertHC and getCore():getBadHighlitedColor() or getCore():getGoodHighlitedColor();
+            plus = "+";
+        end
+        text = string.format("%d (%s%d)", new * multiplicationFactor, plus,
+            new * multiplicationFactor - previous * multiplicationFactor);
     end
 
-    tooltipContext:DrawText(tooltipContext:getFont(), label, label_x_position, y_position, 1, 1, 0.8, 1);
-    tooltipContext:DrawText(tooltipContext:getFont(), value, value_x_position, y_position, 1, 1, 0.8, 1);
-end
-
-function GetValue(value)
-    if value == nil then
-        return nil;
-    elseif value < 1 then
-        return tostring(math.floor((value * 100) + 0.5));
-    else
-        return value;
-    end
+    tooltipContext:DrawText(tooltipContext:getFont(), getText(labelTooltip), label_x_position, y_position, hc:getR(),
+        hc:getG(), hc:getB(), 1);
+    tooltipContext:DrawText(tooltipContext:getFont(), text, value_x_position, y_position, hc:getR(), hc:getG(),
+        hc:getB(), 1);
 end
