e12a0f3f7ba99f9450a3c0bde12b8cf4310a7eef
Frederico Ferreira <fredericodavin@gmail.com>
2023-10-04 18:30:16 -0300

    wip: add forgotten functions

--- Contents/mods/BetterClothingInfoComparison/media/lua/client/bcic_Tooltip.lua
+++ Contents/mods/BetterClothingInfoComparison/media/lua/client/bcic_Tooltip.lua
@@ -1,5 +1,5 @@
 -- author: Bruno Menezes & Fred Davin
--- version: 0.2a (2023-10-03)
+-- version: 0.2a (2023-10-04)
 -- based on: 41+
 
 require "ISUI/ISToolTipInv"
@@ -9,45 +9,182 @@ local original_render = ISToolTipInv.render;
 
 function bcic_DoTooltip(objTooltip, item)
     objTooltip:render();
-    local var2 = objTooltip:getFont();
-    local var3 = objTooltip:getLineSpacing();
-    local var4 = 5;
-    local var6;
-    var6 = item:getName();
-    objTooltip:DrawText(var2, var6, 5.0, var4, 1.0, 1.0, 0.8, 1.0);
+    local tooltipFont = objTooltip:getFont();
+    local tooltipLineSpacing = objTooltip:getLineSpacing();
+    local padding = 5;
 
-    objTooltip:adjustWidth(5, var6);
-    var4 = var4 + var3 + 5;
+    local itemName = item:getName();
+    objTooltip:DrawText(tooltipFont, itemName, 5.0, padding, 1.0, 1.0, 0.8, 1.0);
+
+    objTooltip:adjustWidth(5, itemName);
+    padding = padding + tooltipLineSpacing + 5;
 
     local layoutTooltip = objTooltip:beginLayout();
     layoutTooltip:setMinLabelWidth(80);
     local layout = layoutTooltip:addItem();
     layout:setLabel(getText("Tooltip_item_Weight") .. ":", 1, 1, 0.8, 1);
-    local var16 = item:isEquipped();
-    local var17;
-    local var10001;
-    if not item:IsWeapon() and not item:IsClothing() and not item:IsDrainable() and
-        not string.match(item:getFullType(), "Walkie") then
-        var17 = item:getUnequippedWeight();
-        if var17 > 0.0 and var17 < 0.01 then
-            var17 = 0.01;
-        end
-        layout:setValueRightNoPlus(var17);
-    elseif var16 then
-        var10001 = item:getCleanString(item:getEquippedWeight());
-        layout:setValue(var10001 .. "    (" .. item:getCleanString(item:getUnequippedWeight()) .. " " ..
+
+    if item:isEquipped() then
+        local equippedWeight = item:getCleanString(item:getEquippedWeight());
+        layout:setValue(equippedWeight .. "    (" .. item:getCleanString(item:getUnequippedWeight()) .. " " ..
             getText("Tooltip_item_Unequipped") .. ")", 1.0, 1.0, 1.0, 1.0);
     elseif item:getAttachedSlot() > -1 then
-        var10001 = item:getCleanString(item:getHotbarEquippedWeight());
-        layout:setValue(var10001 .. "    (" .. item:getCleanString(item:getUnequippedWeight()) .. " " ..
+        local equippedWeight = item:getCleanString(item:getHotbarEquippedWeight());
+        layout:setValue(equippedWeight .. "    (" .. item:getCleanString(item:getUnequippedWeight()) .. " " ..
             getText("Tooltip_item_Unequipped") .. ")", 1.0, 1.0, 1.0, 1.0);
     else
-        var10001 = item:getCleanString(item:getUnequippedWeight());
-        layout:setValue(var10001 .. "    (" .. item:getCleanString(item:getEquippedWeight()) .. " " ..
+        local unequippedWeight = item:getCleanString(item:getUnequippedWeight());
+        layout:setValue(unequippedWeight .. "    (" .. item:getCleanString(item:getEquippedWeight()) .. " " ..
             getText("Tooltip_item_Equipped") .. ")", 1.0, 1.0, 1.0, 1.0);
     end
 
-    DoTooltipClothing(objTooltip, item, layoutTooltip)
+    local weightOfStack = objTooltip:getWeightOfStack();
+    if weightOfStack > 0.0 then
+        layout = layoutTooltip:addItem();
+        layout:setLabel(getText("Tooltip_item_StackWeight") .. ":", 1.0, 1.0, 0.8, 1.0);
+        
+        if weightOfStack > 0.0 and weightOfStack < 0.01 then
+            weightOfStack = 0.01;
+        end
+
+        layout:setValueRightNoPlus(weightOfStack);
+    end
+
+    -- if (Core.bDebug && DebugOptions.instance.TooltipInfo.getValue()) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel("getActualWeight()", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.getActualWeight());
+    --     var15 = var14.addItem();
+    --     var15.setLabel("getWeight()", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.getWeight());
+    --     var15 = var14.addItem();
+    --     var15.setLabel("getEquippedWeight()", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.getEquippedWeight());
+    --     var15 = var14.addItem();
+    --     var15.setLabel("getUnequippedWeight()", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.getUnequippedWeight());
+    --     var15 = var14.addItem();
+    --     var15.setLabel("getContentsWeight()", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.getContentsWeight());
+    --     if (this instanceof Key || "Doorknob".equals(this.type)) {
+    --         var15 = var14.addItem();
+    --         var15.setLabel("DBG: keyId", 1.0F, 1.0F, 0.8F, 1.0F);
+    --         var15.setValueRightNoPlus(this.getKeyId());
+    --     }
+
+    --     var15 = var14.addItem();
+    --     var15.setLabel("ID", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.id);
+    --     var15 = var14.addItem();
+    --     var15.setLabel("DictionaryID", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRightNoPlus(this.registry_id);
+    --     ClothingItem var21 = this.getClothingItem();
+    --     if (var21 != null) {
+    --         var15 = var14.addItem();
+    --         var15.setLabel("ClothingItem", 1.0F, 1.0F, 1.0F, 1.0F);
+    --         var15.setValue(this.getClothingItem().m_Name, 1.0F, 1.0F, 1.0F, 1.0F);
+    --     }
+    -- }
+
+    -- if (this.getFatigueChange() != 0.0F) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_item_Fatigue") + ": ", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValueRight((int)(this.getFatigueChange() * 100.0F), false);
+    -- }
+
+    -- ColorInfo var11;
+    -- if (this instanceof DrainableComboItem) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("IGUI_invpanel_Remaining") + ": ", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var17 = ((DrainableComboItem)this).getUsedDelta();
+    --     var11 = new ColorInfo();
+    --     Core.getInstance().getBadHighlitedColor().interp(Core.getInstance().getGoodHighlitedColor(), var17, var11);
+    --     var15.setProgress(var17, var11.getR(), var11.getG(), var11.getB(), 1.0F);
+    -- }
+
+    -- if (this.isTaintedWater() && SandboxOptions.instance.EnableTaintedWaterText.getValue()) {
+    --     var15 = var14.addItem();
+    --     if (this.isCookable()) {
+    --         var15.setLabel(Translator.getText("Tooltip_item_TaintedWater"), 1.0F, 0.5F, 0.5F, 1.0F);
+    --     } else {
+    --         var15.setLabel(Translator.getText("Tooltip_item_TaintedWater_Plastic"), 1.0F, 0.5F, 0.5F, 1.0F);
+    --     }
+    -- }
+
+    DoTooltipClothing(objTooltip, item, layoutTooltip);
+
+    -- if (this.getRemoteControlID() != -1) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_TrapControllerID"), 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValue(Integer.toString(this.getRemoteControlID()), 1.0F, 1.0F, 0.8F, 1.0F);
+    -- }
+
+    -- if (!FixingManager.getFixes(this).isEmpty()) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_weapon_Repaired") + ":", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     if (this.getHaveBeenRepaired() == 1) {
+    --         var15.setValue(Translator.getText("Tooltip_never"), 1.0F, 1.0F, 1.0F, 1.0F);
+    --     } else {
+    --         var15.setValue(this.getHaveBeenRepaired() - 1 + "x", 1.0F, 1.0F, 1.0F, 1.0F);
+    --     }
+    -- }
+
+    -- if (this.isEquippedNoSprint()) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_CantSprintEquipped"), 1.0F, 0.1F, 0.1F, 1.0F);
+    -- }
+
+    -- if (this.isWet()) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_Wetness") + ": ", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var17 = this.getWetCooldown() / 10000.0F;
+    --     var11 = new ColorInfo();
+    --     Core.getInstance().getGoodHighlitedColor().interp(Core.getInstance().getBadHighlitedColor(), var17, var11);
+    --     var15.setProgress(var17, var11.getR(), var11.getG(), var11.getB(), 1.0F);
+    -- }
+
+    -- if (this.getMaxCapacity() > 0) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_container_Capacity") + ":", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var17 = (float)this.getMaxCapacity();
+    --     if (this.isConditionAffectsCapacity()) {
+    --         var17 = VehiclePart.getNumberByCondition((float)this.getMaxCapacity(), (float)this.getCondition(), 5.0F);
+    --     }
+
+    --     if (this.getItemCapacity() > -1.0F) {
+    --         var15.setValue(this.getItemCapacity() + " / " + var17, 1.0F, 1.0F, 0.8F, 1.0F);
+    --     } else {
+    --         var15.setValue("0 / " + var17, 1.0F, 1.0F, 0.8F, 1.0F);
+    --     }
+    -- }
+
+    -- if (this.getConditionMax() > 0 && this.getMechanicType() > 0) {
+    --     var15 = var14.addItem();
+    --     var15.setLabel(Translator.getText("Tooltip_weapon_Condition") + ":", 1.0F, 1.0F, 0.8F, 1.0F);
+    --     var15.setValue(this.getCondition() + " / " + this.getConditionMax(), 1.0F, 1.0F, 0.8F, 1.0F);
+    -- }
+
+    -- if (this.isRecordedMedia()) {
+    --     MediaData var22 = this.getMediaData();
+    --     if (var22 != null) {
+    --         if (var22.getTranslatedTitle() != null) {
+    --             var15 = var14.addItem();
+    --             var15.setLabel(Translator.getText("Tooltip_media_title") + ":", 1.0F, 1.0F, 0.8F, 1.0F);
+    --             var15.setValue(var22.getTranslatedTitle(), 1.0F, 1.0F, 1.0F, 1.0F);
+    --             if (var22.getTranslatedSubTitle() != null) {
+    --                 var15 = var14.addItem();
+    --                 var15.setLabel("", 1.0F, 1.0F, 0.8F, 1.0F);
+    --                 var15.setValue(var22.getTranslatedSubTitle(), 1.0F, 1.0F, 1.0F, 1.0F);
+    --             }
+    --         }
+
+    --         if (var22.getTranslatedAuthor() != null) {
+    --             var15 = var14.addItem();
+    --             var15.setLabel(Translator.getText("Tooltip_media_author") + ":", 1.0F, 1.0F, 0.8F, 1.0F);
+    --             var15.setValue(var22.getTranslatedAuthor(), 1.0F, 1.0F, 1.0F, 1.0F);
+    --         }
+    --     }
+    -- }
 
     if getCore():getOptionShowItemModInfo() and not item:isVanilla() then
         layout = layoutTooltip:addItem();
@@ -76,10 +213,10 @@ function bcic_DoTooltip(objTooltip, item)
         layout:setLabel(getText(item:getTooltip()), 1.0, 1.0, 0.8, 1.0);
     end
 
-    var4 = layoutTooltip:render(5, var4, objTooltip);
+    padding = layoutTooltip:render(5, padding, objTooltip);
     objTooltip:endLayout(layoutTooltip);
-    var4 = var4 + 5;
-    objTooltip:setHeight(var4);
+    padding = padding + 5;
+    objTooltip:setHeight(padding);
     if objTooltip:getWidth() < 150.0 then
         objTooltip:setWidth(150.0);
     end
